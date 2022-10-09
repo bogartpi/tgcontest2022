7407807cf78f3041fbcb5ffaa84baf8a67bb9f4e
Pavel <pavel.bogart@whiparound.com>
2022-10-09 20:37:50 +0300

    Initial Commit

---
+++ TGContest2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TGContest2022
+//
+//  Created by Pavel Bogart on 09.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
